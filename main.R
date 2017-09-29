7885fb6e21dec81da9dd754b4d2717550f0dde36
fjaite <federico.jaite@gmail.com>
2017-09-29 20:42:24 -0300

    Lectura del CSV

--- main.R
+++ main.R
@@ -1,24 +1,32 @@
-paises_nombres =c("japon","china","usa","italia","nueva_zelanda","turquia","taiwan","nepal")
-
-
-anios=c(2011,1995,2008,1994,1980,1976,2011,2004,1999,2010,2012,1999,2015,1906,1923,1989)
-paises =c(1, 1, 2, 3, 4, 2, 5, 1, 6, 7, 4, 8, 9, 3, 1, 3)
-escalas= c(9.1,6.9,8.0,6.7,6.9,7.8,6.3,6.8,7.6,8.8,6.1,7.6,7.8,7.8,7.9,6.9)
+setwd("/home/kitematic/ps/")
+dat = read.csv("dataset.csv", header = TRUE, sep = ';')
+anios=dat$YEAR
+paises =dat$COUNTRY
+escalas= dat$ESCALA
 
 plot(paises, escalas)
-
-
-
 plot(anios,escalas)
-
-
 plot(anios, escalas, xlim=range(anios), ylim=range(escalas), xlab="x", ylab="y", 
      main = "noise-less data",pch=16,col="blue")
 lines(anios[order(anios)], escalas[order(anios)], xlim=range(anios), ylim=range(escalas), pch=16,col="red")
-
 
 plot(anios,escalas, type=, col="blue")
 
 summary(escalas)
 hist(escalas)
 
+plot(anios, escalas, type='n')
+points(anios[paises=='CHINA'], escalas[paises=='CHINA'], pch='C')
+
+plot(anios[paises=='CHINA'], escalas[paises=='CHINA'], type = 'l', xlab="Anios", ylab="Escala", main = "China")
+
+# Cantidad de terremotos por año
+plot(data.frame(table(anios)), type='l', col='blue')
+
+# Cantidad de terremotos por pais
+plot(data.frame(table(paises)), type='l', col='blue')
+
+por_pais = data.frame(table(paises))
+
+# TOP 5 Terremotos por pais
+head(por_pais[order(-por_pais$Freq),], 5)
