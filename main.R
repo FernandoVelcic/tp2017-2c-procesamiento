8ab54374e595d0a47fead2f0cb251b089ef84ba0
fjaite <federico.jaite@gmail.com>
2017-11-03 20:50:39 -0300

    Final

--- main.R
+++ main.R
@@ -1,4 +1,3 @@
-setwd("/home/kitematic/ps/")
 dat = read.csv("dataset.csv", header = TRUE, sep = ';')
 anios=dat$YEAR
 paises =dat$COUNTRY
@@ -23,6 +22,14 @@ plot(anios[paises=='CHINA'], escalas[paises=='CHINA'], type = 'l', xlab="Anios",
 # Cantidad de terremotos por año
 plot(data.frame(table(anios)), type='l', col='blue', xlab="Anio", ylab="Cantidad", main='Terremotos por año')
 
+# Acumulado de magnitudes
+acum = aggregate( ESCALA ~ YEAR, dat, sum)
+plot(acum, type = 'l', xlab="Anio", ylab="Acumulado", main='Acumulado de magnitudes por año' )
+
+# Media de magnitudes
+media = aggregate( ESCALA ~ YEAR, dat, mean)
+plot(media, type = 'l', xlab="Anio", ylab="Media", main='Media de magnitudes por año' )
+
 # Cantidad de terremotos por pais
 plot(data.frame(table(paises)), type='l', col='blue')
 
@@ -30,3 +37,6 @@ por_pais = data.frame(table(paises))
 
 # TOP 5 Terremotos por pais
 head(por_pais[order(-por_pais$Freq),], 5)
+
+acum_por_pais = aggregate( ESCALA ~ COUNTRY, dat, sum)
+head(acum_por_pais[order(-acum_por_pais$ESCALA),], 5)
